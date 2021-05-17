64e778521781c2d082df04239f08df1287e2cc09
Noah Irving <nji29@uclive.ac.nz>
2021-05-17 17:00:29 +1200

    Added events and axios

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import Events from '../views/Events.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -14,7 +15,13 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/events',
+    name: 'Events',
+    component: Events
   }
+
 ]
 
 const router = createRouter({
---
+++ src/views/Events.vue
@@ -0,0 +1,36 @@
+<template>
+<h1>Events</h1>
+
+  <ol>
+    <li v-for="event in events" :key="event.id">
+      {{event.title}}
+    </li>
+  </ol>
+
+</template>
+
+<script>
+export default {
+  name: "Events",
+  data() {
+    return {
+      events: []
+    }
+  },
+  methods: {
+    async getEvents() {
+      const {status, data} = await this.axios.get("events");
+      if (status === 200) {
+        this.events = data;
+      }
+    }
+  },
+  mounted() {
+    this.getEvents();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
